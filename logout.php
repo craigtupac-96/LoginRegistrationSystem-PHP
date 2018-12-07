6949d295acc5483e3fd716799805b65b92d20f1f
craigtupac-96 <Siberia1#>
2018-12-07 23:00:20 +0000

    done

--- logout.php
+++ logout.php
@@ -1,9 +1,4 @@
 <?php
     session_start();
-    include('connect.php');
-    $username = $_SESSION['username'];
-    $false = 0;
-    $sql = "UPDATE sessions SET loggedIn='$false' WHERE username='$username'";
-    mysqli_query($con, $sql);
     session_destroy();
     header('location: loginForm.php');
